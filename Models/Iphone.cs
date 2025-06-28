63eb854180367aa5827962d2fe0151e27a61ff41
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2025-06-28 19:21:34 -0300

    feat: completa desafio.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string aplicativo)
+        {
+            Console.WriteLine($"Instalando o aplicativo {aplicativo} no iPhone...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testes com Nokia:");
+Smartphone nokia = new Nokia(numero: "931234567", modelo: "Nokia 3310", imei: "1234567890", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("\nTestes com iPhone:");
+Smartphone iphone = new Iphone(numero: "912345678", modelo: "iPhone 14", imei: "0987654321", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
